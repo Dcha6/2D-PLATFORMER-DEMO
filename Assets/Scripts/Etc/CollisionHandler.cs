5d20ddbf5282f124945e70643aa62b4a367a9c39
David <chadavid472@gmail.com>
2024-05-26 00:12:27 -0400

    Cleaning up

--- Assets/Scripts/Etc/CollisionHandler.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class CollisionHandler : MonoBehaviour
-{
-    public string sceneToLoad;
-    public string spawnPointName;
-}
